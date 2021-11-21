7737351ec4ecfdc37c3f21bf2ee7c2b352ecf890
[name] <[waad.awad.eam.js@tuwaiq.edu.sa]>
2021-11-21 17:51:16 +0300

    added data file

---
+++ backend/Routers/routes/data.js
@@ -0,0 +1,104 @@
+const user = [
+    {
+        id:1,
+       firstname:"Abeer",
+        lastname:"Abdullah",
+        email:"a1418abeer@gmail.com",
+        numberphone: 8274803873,
+        age:18,
+        city:"najran",
+        platoon:"o-"
+    },
+    {
+        id:2,
+       firstname:"Waad",
+        lastname:"Awad",
+        email:"ask23@gmail.com",
+        numberphone: 5639734344,
+        age:23,
+        city:"Abha",
+        platoon:"A+"
+    },
+    {
+        id:3,
+        firstname:"Hamud",
+         lastname:"Alrezg",
+         email:"tsgv69@gmail.com",
+         numberphone: 9873673223,
+         age:43,
+         city:"Riyadh",
+         platoon:"o+"
+    },
+    {
+        id:4,
+       firstname:"Mohmmad",
+        lastname:"Ali",
+        email:"gaming@gmail.com",
+        numberphone: 1234532174,
+        age:35,
+        city:"makah",
+        platoon:"A-"
+    },
+    {
+        id:5,
+        firstname:"majed",
+         lastname:"saeed",
+         email:"qwe123@gmail.com",
+         numberphone: 1273829105,
+         age:29,
+         city:"Jeddah",
+         platoon:"AB+"
+    },
+    {
+        id:6,
+        firstname:"muna",
+         lastname:"Ahmad",
+         email:"asd345@gmail.com",
+         numberphone: 5729398723,
+         age:39,
+         city:"Tabuk",
+         platoon:"B-"
+    },
+    {
+        id:7,
+        firstname:"talal",
+         lastname:"Ahmad",
+         email:"asd34@gmail.com",
+         numberphone: 5729398,
+         age:38,
+         city:"Tabuk",
+         platoon:"B-"
+        
+    },
+    {
+        id:8,
+        firstname:"mohammed",
+         lastname:"Ahmad",
+         email:"asd345@gmail.com",
+         numberphone: 12329398723,
+         age:40,
+         city:"Abha",
+         platoon:"O+"
+    },
+    {
+        id:9,
+        firstname:"ali",
+         lastname:"salem",
+         email:"Basd345@gmail.com",
+         numberphone: 0257245,
+         age:39,
+         city:"Abha",
+         platoon:"A-"
+    },
+
+
+
+
+]
+module.exports = {user}
+
+
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-
 //routers
-
+const {userRouter} = require("./Routers/routes/userRouter")
 //built-in middlewares
 app.use(express.json());
-
 //third-party middleware
 app.use(cors());
-
 //app routers
-
-const PORT = process.env.PORT || 5000;
-
+app.use('/users', userRouter);
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-	console.log(`Server On ${PORT}`);
+    console.log(`Server On ${PORT}`);
 });
