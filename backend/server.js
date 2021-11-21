14de1c10b140f2c0addc8c70c6cde19cf31f62dd
[hamudalrezg] <[hamud.alrzg.eam.js@tuwaiq.edu.sa]>
2021-11-21 17:43:32 +0300

    added users database

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 
 //routers
+const {userRouter} = require("./Routers/routes/userRouter")
 
 //built-in middlewares
 app.use(express.json());
@@ -12,8 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 //app routers
+app.use('/users', userRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
 	console.log(`Server On ${PORT}`);
