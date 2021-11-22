2b5f051fdcee13759aff73a6123f93afeb83b532
[hamudalrezg] <[hamud.alrzg.eam.js@tuwaiq.edu.sa]>
2021-11-22 13:55:34 +0300

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
 
-const App = () => {
-	return <div className="App">App component</div>;
-};
 
-export default App;
+import Footer from "./components/footer/Footer";
+
+import "./App.css";
+// import {  Route, Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      {/* <Header />
+      <div>
+      <Routes>
+          <Route path="Body" element={<Body />} />
+           <Route path="ChatLive" element={<ChatLive />} />
+          <Route path="SingUp" element={<SingUp/>}/>
+          <Route path="SingIn" element={<SingIn/>}/>
+          <Route path="Rooms" element={<Rooms/>}/>
+          
+        </Routes>
+      </div> */}
+      <Footer/>
+    </div>
+    
+  );
+}
