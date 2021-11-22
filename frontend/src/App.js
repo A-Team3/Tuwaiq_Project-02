9890dede0264a599ec29604b71ddd98991b1ab57
Abeer1418 <Abeer.Abdullah.eam.js@tuwaiq.edu.sa>
2021-11-22 13:48:42 +0300

    singin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Singin from './components/main/Singin'
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div>
+<Route/>
+		<Singin/>
+	</div>
 };
 
 export default App;
