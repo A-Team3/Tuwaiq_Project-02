61a0d6ea83607d63cbdf09b757de41478c0b01d1
[name] <[waad.awad.eam.js@tuwaiq.edu.sa]>
2021-11-22 13:56:26 +0300

    navBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-
-const App = () => {
-	return <div className="App">App component</div>;
-};
-
-export default App;
+import React from "react";
+import Header from "./components/navigation/Header";
+// import Body from "./routes/Body";
+// import SingUp from "./routes/SingUp";
+// import SingIn from "./routes/SingIn";
+// import Footer from "./routes/Footer";
+// import Rooms from "./routes/Rooms";
+// import ChatLive from "./routes/ChatLive";
+import "./App.css";
+import {  Route, Routes } from "react-router-dom";
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <div>
+     
+      </div>
+    
+    </div>
+  );
+}
