cdc8f08ff4493b6a1f247902fbf240adf485de1f
[hamudalrezg] <[hamud.alrzg.eam.js@tuwaiq.edu.sa]>
2021-11-29 18:09:25 +0300

    ab

--- frontend/src/components/main/SignUp.js
+++ frontend/src/components/main/SignUp.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Form ,Button,Card}from "react-bootstrap";
-import { useState } from 'react';
-import {  useNavigate } from "react-router-dom"
-import axios from 'axios'
+import { Form, Button, Card } from "react-bootstrap";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 //import {Link} from"react-router-dom";
 import "./main.css";
- function SignUp(){
+function SignUp() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -16,147 +16,199 @@ import "./main.css";
   const [bloodGroup, setBloodGroup] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
-  const [age18, setAge18]= useState(false);
-  
-  
+  const [age18, setAge18] = useState(false);
 
-  function handleSubmit(event){
-//  console.log(firstName, lastName, age, email, bloodGroup);
+  function handleSubmit() {
+    //  console.log(firstName, lastName, age, email, bloodGroup);
 
-//Check if age < 18 then show message that must have age above 18
-   event.preventDefault();
+    //Check if age < 18 then show message that must have age above 18
+    //  axios.get('http://localhost:5000/users')
+    // .then(function (response) {
+    //   console.log(response);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+    const data = {
+      id: 100,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      age: age,
+      city: city,
+      bloodGroup: bloodGroup,
+      phoneNumber: phoneNumber,
+    };
+    axios
+      .post("http://localhost:5000/users", data)
+      .then((response) => {
+        //alert("Correct");
+        console.log(response);
+        localStorage.setItem("users", JSON.stringify(data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  const navigate = useNavigate();
+  return (
+    <div style={{ textAlign: "center", marginLeft: "35%", width: "200px" }}>
+      <Card
+        style={{
+          textAlign: "center",
+          marginLeft: "20px",
+          backgroundColor: "rgb(205, 92, 92)",
+          width: "400px",
+        }}
+      >
+        <Card.Body>
+          <h1>You Need SignUp</h1>
 
-
-  //  axios.get('http://localhost:5000/users')
-  // .then(function (response) {
-  //   console.log(response);
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // });
-axios.post('http://localhost:5000/users', {
-  firstName: firstName,
-  lastName: lastName,
-  email: email,
-  password: password,
-  age: age,
-  city:city,
-  bloodGroup:bloodGroup,
-  phoneNumber: phoneNumber,
-
-})
-.then(function (response) {
-  console.log(response);
-})
-.catch(function (error) {
-  console.log(error);
-});
-
-}
-const navigate = useNavigate()
-  return( 
-     
-     <div style={{textAlign:"center",marginLeft:"35%",width:"200px"}}>
-      
-       <Card style={{textAlign:"center",marginLeft:"20px",backgroundColor: "rgb(205, 92, 92)",width:"400px"}}>
-  <Card.Body>
-     
-     
-       <h1>You Need SignUp</h1>
-    
-     
-<Form dir-align="center" onSubmit={e => { handleSubmit(e) }}>
-
-<Form.Group className="mb-3" controlId="formBasicFirstName">
-    <Form.Label>First Name</Form.Label>
-    <Form.Control type="firstName" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="Enter First Name" />
-    {/* <input 
+          <Form
+            dir-align="center"
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            <Form.Group className="mb-3" controlId="formBasicFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="firstName"
+                name="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="Enter First Name"
+              />
+              {/* <input 
           name='firstName' 
           type='text'
           value={firstName}
           onChange={e => setFirstName(e.target.value)}
         /> */}
-    {/* <Form.Control type="phoneNummber" name="phoneNumber" value={phoneNumber} onChange={e => handleChange(e.target.value)} placeholder="Enter Phone Number" /> */}
+              {/* <Form.Control type="phoneNummber" name="phoneNumber" value={phoneNumber} onChange={e => handleChange(e.target.value)} placeholder="Enter Phone Number" /> */}
+            </Form.Group>
 
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicLastName">
-    <Form.Label>Last Name</Form.Label>
-    <Form.Control type="lastName" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="Enter Last Name" />
-    {/* <input 
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="lastName"
+                name="lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Enter Last Name"
+              />
+              {/* <input 
           name='firstName' 
           type='text'
           value={firstName}
           
         /> */}
+            </Form.Group>
 
-  </Form.Group>
-  
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter email" />
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter email"
+              />
+            </Form.Group>
 
-  </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicAge">
+              <Form.Label>Age</Form.Label>
+              <Form.Control
+                type="age"
+                name="age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+                placeholder="Enter Age"
+              />
+            </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicAge">
-    <Form.Label>Age</Form.Label>
-    <Form.Control type="age" name="age" value={age} onChange={e => setAge(e.target.value)} placeholder="Enter Age" />
+            <Form.Group className="mb-3" controlId="formBasicNumberPhone">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                type="phoneNumber"
+                name="phoneNumber"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                placeholder="Enter Phone Number"
+              />
+            </Form.Group>
 
-  </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCity">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="city"
+                name="city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="Enter City"
+              />
+            </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicNumberPhone">
-    <Form.Label>Phone Number</Form.Label>
-    <Form.Control type="phoneNumber" name="phoneNumber" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} placeholder="Enter Phone Number" />
-  </Form.Group>
-  
-  <Form.Group className="mb-3" controlId="formBasicCity">
-    <Form.Label>City</Form.Label>
-    <Form.Control type="city" name="city" value={city} onChange={e => setCity(e.target.value)} placeholder="Enter City" />
-  </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPlatoon">
+              <Form.Label>Blood Group</Form.Label>
+              <Form.Control
+                type="bloodGroup"
+                name="bloodGroup"
+                value={bloodGroup}
+                onChange={(e) => setBloodGroup(e.target.value)}
+                placeholder="Enter Blood Group"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPlatoon">
+              <Form.Label>password</Form.Label>
+              <Form.Control
+                type="Password"
+                name="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPlatoon">
-    <Form.Label>Blood Group</Form.Label>
-    <Form.Control type="bloodGroup" name="bloodGroup" value={bloodGroup} onChange={e => setBloodGroup(e.target.value)} placeholder="Enter Blood Group" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicPlatoon">
-    <Form.Label>password</Form.Label>
-    <Form.Control type="Password" name="Password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
-  </Form.Group>
-  
-  {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
   </Form.Group> */}
-  <Button style={{backgroundColor: "rgb(68, 53, 53)"}} onClick={()=>{
-    if(age >= 18){
-      navigate("/SignIn")
-    }else{
-      setAge18(true);
-    }
-  }}>
-    Submit
-  </Button>
-</Form>
-</Card.Body>
-</Card>
-    
-     {age18 &&
-    <center>
-     <span
-     style={{
-       display: "block",
-       margin: "auto",
-       textAlign: "center",
-       fontSize: "large",
-       fontStyle: "bold",
-       color: "red",
-       
-     }}
-   >
-   You Must Be an Adult
-   </span>
-   </center>
-   }
-   </div>
-   )
- }
- export default SignUp;
+            <Button
+              style={{ backgroundColor: "rgb(68, 53, 53)" }}
+              onClick={() => {
+                if (age >= 18) {
+                  navigate("/SignIn");
+                } else {
+                  setAge18(true);
+                  return;
+                }
+                handleSubmit();
+              }}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+
+      {age18 && (
+        <center>
+          <span
+            style={{
+              display: "block",
+              margin: "auto",
+              textAlign: "center",
+              fontSize: "large",
+              fontStyle: "bold",
+              color: "red",
+            }}
+          >
+            You Must Be an Adult
+          </span>
+        </center>
+      )}
+    </div>
+  );
+}
+export default SignUp;
--- frontend/src/components/main/Success.js
+++ frontend/src/components/main/Success.js
@@ -1,17 +1,55 @@
 import React from "react";
 
-import {Card,Button} from "react-bootstrap";
+import {Card,Button, Form, Row, Col, Container} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
  import { Link } from "react-router-dom"
-import "./main.css"
+import "./main.css";
+import { useState } from "react";
 
  export default function Success(){
+    const [hospital, setHospital]= useState("");
+    const [date, setDate]= useState("");
+
+    // function saveAppoitment(){
+        const obj={hospital:hospital, date: date}
+      localStorage.setItem("Appoitment",JSON.stringify(obj) )
+        
+      
+
+    // }
+
+    // console.log(save);
      return(
-
+      // backgroundImage: `url("https://i.pinimg.com/564x/81/45/01/814501f62677f65ae78d9288be124c0b.jpg")`
         <div style={{textAlign:"center",padding:"50px",
-            backgroundImage: `url("https://i.pinimg.com/564x/81/45/01/814501f62677f65ae78d9288be124c0b.jpg")`,width:"100%",height:"100vh", backgroundRepeat:"no-repeat", backgroundPosition:"center"}}>
-          <h1 id="background-text"> Congratulations, you are qualified!</h1>
+           width:"100%",height:"100vh", backgroundRepeat:"no-repeat", backgroundPosition:"center"}}>
+             <Container>
+ 
+  <Row>
+    <Col></Col>
+    <Col>   <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Name of Hospital</Form.Label>
+    <Form.Control type="text" placeholder="Enter hospital" onChange={(e)=>setHospital(e.target.value) }/>
+    
+  </Form.Group>
 
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Reservation date</Form.Label>
+    <Form.Control type="date" placeholder="Enter date"  onChange={(e)=>setDate(e.target.value)}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+   
+  </Form.Group>
+  <Button href="/" variant="primary" type="button">
+    Submit
+  </Button>
+</Form></Col>
+    <Col></Col>
+  </Row>
+</Container>
+          {/* <h1 id="background-text"> Congratulations, you are qualified!</h1> */}
+       
             
         </div>
      )
