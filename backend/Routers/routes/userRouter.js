fb78655241a3a02f932da8078c99df43dca1cfec
[hamudalrezg] <[hamud.alrzg.eam.js@tuwaiq.edu.sa]>
2021-11-30 19:11:00 +0300

    done

--- backend/Routers/routes/userRouter.js
+++ backend/Routers/routes/userRouter.js
@@ -3,9 +3,10 @@ const userRouter = express.Router();
 const {getUser,getAllUser,updateUser,addNewUser, loginUser, getBloodTypePeople} = require("../controllers/user");
 
 const { user } = require("./data");
+
 userRouter.get('/',getAllUser);
 userRouter.get('/:id',getUser);
-userRouter.post('/',addNewUser);
+userRouter.post('/', addNewUser);
 userRouter.post('/login', loginUser);
 userRouter.put('/user',updateUser);
 userRouter.post('/people', getBloodTypePeople);
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-//routers
-const {userRouter} = require("./Routers/routes/userRouter")
-//built-in middlewares
-app.use(express.json());
-//third-party middleware
-app.use(cors());
-//app routers
 
-app.use('/users', userRouter);
-// new changes
+const { userRouter } = require("./Routers/routes/userRouter");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/users", userRouter);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server On ${PORT}`);
+  console.log(`Server On ${PORT}`);
 });
